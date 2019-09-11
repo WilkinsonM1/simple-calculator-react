05c9fbcd2b6a3ea873d52923ceef089ccdd91882
System Administrator <root@CN72.local>
2019-09-11 14:51:30 +0100

    Updates

---
+++ precache-manifest.520a00686dd97fe8659295b98bbdaad8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8a39fd4ea2bd7d1953d02c596ebdbc",
+    "url": "/simple-calculator-react/./index.html"
+  },
+  {
+    "revision": "3357308ef416402ebc77",
+    "url": "/simple-calculator-react/./static/css/main.8fc13d75.chunk.css"
+  },
+  {
+    "revision": "9018344e5884c4a69299",
+    "url": "/simple-calculator-react/./static/js/2.19f7ca3a.chunk.js"
+  },
+  {
+    "revision": "3357308ef416402ebc77",
+    "url": "/simple-calculator-react/./static/js/main.89b1fba1.chunk.js"
+  },
+  {
+    "revision": "948bc2f0bd0811653499",
+    "url": "/simple-calculator-react/./static/js/runtime~main.d3519d88.js"
+  }
+]);
